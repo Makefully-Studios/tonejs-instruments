c1aabb53415f50a73f5f461b9a1e4a8050279471
Derek Detweiler <derek.detweiler@gmail.com>
2022-03-29 10:57:40 -0400

    Fixed up trumpet octaves per recommendation here. https://github.com/nbrosowsky/tonejs-instruments/issues/16

--- samples/trumpet/ogg/index.js
+++ samples/trumpet/ogg/index.js
@@ -1,44 +1,44 @@
 import {Sampler} from 'tone';
 
 // Audio Files
-import A2 from './A2.ogg';
-import A4 from './A4.ogg';
+import A3 from './A3.ogg';
+import A5 from './A5.ogg';
 import As3 from './As3.ogg';
 import C3 from './C3.ogg';
-import C5 from './C5.ogg';
-import D4 from './D4.ogg';
-import Ds3 from './Ds3.ogg';
-import F2 from './F2.ogg';
+import C6 from './C6.ogg';
+import D5 from './D5.ogg';
+import Ds4 from './Ds4.ogg';
 import F3 from './F3.ogg';
 import F4 from './F4.ogg';
-import G3 from './G3.ogg';
+import F5 from './F5.ogg';
+import G4 from './G4.ogg';
 
 const
   AUDIO = {
-    "A2": A2,
-    "A4": A4,
+    "A3": A3,
+    "A5": A5,
     "A#3": As3,
     "C3": C3,
-    "C5": C5,
-    "D4": D4,
-    "D#3": Ds3,
-    "F2": F2,
+    "C6": C6,
+    "D5": D5,
+    "D#4": Ds4,
     "F3": F3,
     "F4": F4,
-    "G3": G3
+    "F5": F5,
+    "G4": G4
   },
   AUDIO_MIN = {
-    "A2": A2,
-    "A4": A4,
+    "A3": A3,
+    "A5": A5,
     "A#3": As3,
     "C3": C3,
-    "C5": C5,
-    "D4": D4,
-    "D#3": Ds3,
-    "F2": F2,
+    "C6": C6,
+    "D5": D5,
+    "D#4": Ds4,
     "F3": F3,
     "F4": F4,
-    "G3": G3
+    "F5": F5,
+    "G4": G4
   };
 
 export default class InstrumentTrumpetOgg extends Sampler {
--- samples/trumpet/wav/index.js
+++ samples/trumpet/wav/index.js
@@ -1,44 +1,44 @@
 import {Sampler} from 'tone';
 
 // Audio Files
-import A2 from './A2.wav';
-import A4 from './A4.wav';
+import A3 from './A3.wav';
+import A5 from './A5.wav';
 import As3 from './As3.wav';
 import C3 from './C3.wav';
-import C5 from './C5.wav';
-import D4 from './D4.wav';
-import Ds3 from './Ds3.wav';
-import F2 from './F2.wav';
+import C6 from './C6.wav';
+import D5 from './D5.wav';
+import Ds4 from './Ds4.wav';
 import F3 from './F3.wav';
 import F4 from './F4.wav';
-import G3 from './G3.wav';
+import F5 from './F5.wav';
+import G4 from './G4.wav';
 
 const
   AUDIO = {
-    "A2": A2,
-    "A4": A4,
+    "A3": A3,
+    "A5": A5,
     "A#3": As3,
     "C3": C3,
-    "C5": C5,
-    "D4": D4,
-    "D#3": Ds3,
-    "F2": F2,
+    "C6": C6,
+    "D5": D5,
+    "D#4": Ds4,
     "F3": F3,
     "F4": F4,
-    "G3": G3
+    "F5": F5,
+    "G4": G4
   },
   AUDIO_MIN = {
-    "A2": A2,
-    "A4": A4,
+    "A3": A3,
+    "A5": A5,
     "A#3": As3,
     "C3": C3,
-    "C5": C5,
-    "D4": D4,
-    "D#3": Ds3,
-    "F2": F2,
+    "C6": C6,
+    "D5": D5,
+    "D#4": Ds4,
     "F3": F3,
     "F4": F4,
-    "G3": G3
+    "F5": F5,
+    "G4": G4
   };
 
 export default class InstrumentTrumpetWav extends Sampler {
