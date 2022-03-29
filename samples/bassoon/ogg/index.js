37735d666bcc4dd2036a6d8cc8d6fa92ce3216d4
Derek Detweiler <derek.detweiler@gmail.com>
2022-03-29 10:32:51 -0400

    Fixed up bassoon octaves per recommendation here. https://github.com/nbrosowsky/tonejs-instruments/issues/16

--- samples/bassoon/ogg/index.js
+++ samples/bassoon/ogg/index.js
@@ -1,41 +1,41 @@
 import {Sampler} from 'tone';
 
 // Audio Files
-import A1 from './A1.ogg';
 import A2 from './A2.ogg';
 import A3 from './A3.ogg';
-import C2 from './C2.ogg';
+import A4 from './A4.ogg';
 import C3 from './C3.ogg';
 import C4 from './C4.ogg';
-import E3 from './E3.ogg';
-import G1 from './G1.ogg';
+import C5 from './C5.ogg';
+import E4 from './E4.ogg';
 import G2 from './G2.ogg';
 import G3 from './G3.ogg';
+import G4 from './G4.ogg';
 
 const
   AUDIO = {
-    "A1": A1,
     "A2": A2,
     "A3": A3,
-    "C2": C2,
+    "A4": A4,
     "C3": C3,
     "C4": C4,
-    "E3": E3,
-    "G1": G1,
+    "C5": C5,
+    "E4": E4,
     "G2": G2,
-    "G3": G3
+    "G3": G3,
+    "G4": G4
   },
   AUDIO_MIN = {
-    "A1": A1,
     "A2": A2,
     "A3": A3,
-    "C2": C2,
+    "A4": A4,
     "C3": C3,
     "C4": C4,
-    "E3": E3,
-    "G1": G1,
+    "C5": C5,
+    "E4": E4,
     "G2": G2,
-    "G3": G3
+    "G3": G3,
+    "G4": G4
   };
 
 export default class InstrumentBassoonOgg extends Sampler {
--- samples/bassoon/wav/index.js
+++ samples/bassoon/wav/index.js
@@ -1,41 +1,41 @@
 import {Sampler} from 'tone';
 
 // Audio Files
-import A1 from './A1.wav';
 import A2 from './A2.wav';
 import A3 from './A3.wav';
-import C2 from './C2.wav';
+import A4 from './A4.wav';
 import C3 from './C3.wav';
 import C4 from './C4.wav';
-import E3 from './E3.wav';
-import G1 from './G1.wav';
+import C5 from './C5.wav';
+import E4 from './E4.wav';
 import G2 from './G2.wav';
 import G3 from './G3.wav';
+import G4 from './G4.wav';
 
 const
   AUDIO = {
-    "A1": A1,
     "A2": A2,
     "A3": A3,
-    "C2": C2,
+    "A4": A4,
     "C3": C3,
     "C4": C4,
-    "E3": E3,
-    "G1": G1,
+    "C5": C5,
+    "E4": E4,
     "G2": G2,
-    "G3": G3
+    "G3": G3,
+    "G4": G4
   },
   AUDIO_MIN = {
-    "A1": A1,
     "A2": A2,
     "A3": A3,
-    "C2": C2,
+    "A4": A4,
     "C3": C3,
     "C4": C4,
-    "E3": E3,
-    "G1": G1,
+    "C5": C5,
+    "E4": E4,
     "G2": G2,
-    "G3": G3
+    "G3": G3,
+    "G4": G4
   };
 
 export default class InstrumentBassoonWav extends Sampler {
