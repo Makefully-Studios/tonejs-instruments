396de11a1a7b3f0b6847926ef51c78815da77740
Derek Detweiler <derek.detweiler@gmail.com>
2022-03-29 10:26:19 -0400

    Fixed up bass-electric octaves per recommendation here. https://github.com/nbrosowsky/tonejs-instruments/issues/16

--- samples/bass-electric/mp3/index.js
+++ samples/bass-electric/mp3/index.js
@@ -1,54 +1,54 @@
 import {Sampler} from 'tone';
 
 // Audio Files
+import As1 from './As1.mp3';
 import As2 from './As2.mp3';
 import As3 from './As3.mp3';
 import As4 from './As4.mp3';
-import As5 from './As5.mp3';
+import Cs1 from './Cs1.mp3';
 import Cs2 from './Cs2.mp3';
 import Cs3 from './Cs3.mp3';
 import Cs4 from './Cs4.mp3';
 import Cs5 from './Cs5.mp3';
-import Cs6 from './Cs6.mp3';
+import E1 from './E1.mp3';
 import E2 from './E2.mp3';
 import E3 from './E3.mp3';
 import E4 from './E4.mp3';
-import E5 from './E5.mp3';
+import G1 from './G1.mp3';
 import G2 from './G2.mp3';
 import G3 from './G3.mp3';
 import G4 from './G4.mp3';
-import G5 from './G5.mp3';
 
 const
   AUDIO = {
+    "A#1": As1,
     "A#2": As2,
     "A#3": As3,
     "A#4": As4,
-    "A#5": As5,
+    "C#1": Cs1,
     "C#2": Cs2,
     "C#3": Cs3,
     "C#4": Cs4,
     "C#5": Cs5,
-    "C#6": Cs6,
+    "E1": E1,
     "E2": E2,
     "E3": E3,
     "E4": E4,
-    "E5": E5,
+    "G1": G1,
     "G2": G2,
     "G3": G3,
-    "G4": G4,
-    "G5": G5
+    "G4": G4
   },
   AUDIO_MIN = {
-    "A#2": As2,
-    "A#4": As4,
-    "C#2": Cs2,
-    "C#4": Cs4,
-    "C#6": Cs6,
-    "E3": E3,
-    "E5": E5,
-    "G3": G3,
-    "G5": G5
+    "A#1": As1,
+    "A#3": As3,
+    "C#1": Cs1,
+    "C#3": Cs3,
+    "C#5": Cs5,
+    "E2": E2,
+    "E4": E4,
+    "G2": G2,
+    "G4": G4
   };
 
 export default class InstrumentBassElectricMp3 extends Sampler {
--- samples/bass-electric/ogg/index.js
+++ samples/bass-electric/ogg/index.js
@@ -1,54 +1,54 @@
 import {Sampler} from 'tone';
 
 // Audio Files
+import As1 from './As1.ogg';
 import As2 from './As2.ogg';
 import As3 from './As3.ogg';
 import As4 from './As4.ogg';
-import As5 from './As5.ogg';
+import Cs1 from './Cs1.ogg';
 import Cs2 from './Cs2.ogg';
 import Cs3 from './Cs3.ogg';
 import Cs4 from './Cs4.ogg';
 import Cs5 from './Cs5.ogg';
-import Cs6 from './Cs6.ogg';
+import E1 from './E1.ogg';
 import E2 from './E2.ogg';
 import E3 from './E3.ogg';
 import E4 from './E4.ogg';
-import E5 from './E5.ogg';
+import G1 from './G1.ogg';
 import G2 from './G2.ogg';
 import G3 from './G3.ogg';
 import G4 from './G4.ogg';
-import G5 from './G5.ogg';
 
 const
   AUDIO = {
+    "A#1": As1,
     "A#2": As2,
     "A#3": As3,
     "A#4": As4,
-    "A#5": As5,
+    "C#1": Cs1,
     "C#2": Cs2,
     "C#3": Cs3,
     "C#4": Cs4,
     "C#5": Cs5,
-    "C#6": Cs6,
+    "E1": E1,
     "E2": E2,
     "E3": E3,
     "E4": E4,
-    "E5": E5,
+    "G1": G1,
     "G2": G2,
     "G3": G3,
-    "G4": G4,
-    "G5": G5
+    "G4": G4
   },
   AUDIO_MIN = {
-    "A#2": As2,
-    "A#4": As4,
-    "C#2": Cs2,
-    "C#4": Cs4,
-    "C#6": Cs6,
-    "E3": E3,
-    "E5": E5,
-    "G3": G3,
-    "G5": G5
+    "A#1": As1,
+    "A#3": As3,
+    "C#1": Cs1,
+    "C#3": Cs3,
+    "C#5": Cs5,
+    "E2": E2,
+    "E4": E4,
+    "G2": G2,
+    "G4": G4
   };
 
 export default class InstrumentBassElectricOgg extends Sampler {
